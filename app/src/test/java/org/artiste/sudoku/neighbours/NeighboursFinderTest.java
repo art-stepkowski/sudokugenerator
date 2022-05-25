19cfd92a9096e9698c5d08505310ba3917edada6
Artur Stępkowski <88000253+art-stepkowski@users.noreply.github.com>
2022-05-25 21:14:11 +0200

    Improve test

--- app/src/test/java/org/artiste/sudoku/neighbours/NeighboursFinderTest.java
+++ app/src/test/java/org/artiste/sudoku/neighbours/NeighboursFinderTest.java
@@ -1,24 +1,38 @@
 package org.artiste.sudoku.neighbours;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.artiste.sudoku.generator.SmartGenerator;
 import org.artiste.sudoku.model.CellCoordinates;
 import org.artiste.sudoku.model.Sudoku;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class NeighboursFinderTest {
-  @Test
-  void should_find_proper_number_of_empty_neighbours() {
+  @ParameterizedTest
+  @MethodSource("prepareDataForFindNumberOfEmptyNeighbours")
+  void should_find_proper_number_of_empty_neighbours(List<CellCoordinates> emptyCells,
+                                                     CellCoordinates targetCell,
+                                                     int expectedNumberOfEmptyNeighbours) {
     Sudoku sudoku = new SmartGenerator().generate();
-    sudoku.fillCell(0, 0, 0);
-    sudoku.fillCell(1, 1, 0);
-    sudoku.fillCell(2, 2, 0);
-    sudoku.fillCell(0, 2, 0);
-    sudoku.fillCell(0, 3, 0);
-    sudoku.fillCell(2, 0, 0);
-    sudoku.fillCell(3, 0, 0);
-    int actual = NeighboursFinder.getNumberOfEmptyNeighbours(sudoku, new CellCoordinates(0, 0));
-    assertThat(actual).isEqualTo(6);
+    emptyCells.forEach(c -> sudoku.fillCell(c.getRow(), c.getColumn(), 0));
+    int actual = NeighboursFinder.getNumberOfEmptyNeighbours(sudoku, targetCell);
+    assertThat(actual).isEqualTo(expectedNumberOfEmptyNeighbours);
+  }
+
+  private static Stream<Arguments> prepareDataForFindNumberOfEmptyNeighbours() {
+    return Stream.of(Arguments.of(List.of(new CellCoordinates(1, 1),
+                                          new CellCoordinates(2, 2),
+                                          new CellCoordinates(1, 2),
+                                          new CellCoordinates(1, 3),
+                                          new CellCoordinates(2, 1),
+                                          new CellCoordinates(3, 1)), new CellCoordinates(1, 1), 5),
+                     Arguments.of(List.of(new CellCoordinates(1, 1)), new CellCoordinates(1, 1), 0),
+                     Arguments.of(List.of(new CellCoordinates(3, 3), new CellCoordinates(3, 4)),
+                                  new CellCoordinates(3, 3),
+                                  1));
   }
 }
